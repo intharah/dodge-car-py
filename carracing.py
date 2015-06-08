8cfb1c3e5b195090d7654b545dd85a09b3e882b9
biou <biou@sous-anneau.org>
2015-06-08 23:20:50 +0200

    added life jauges

--- carracing.py
+++ carracing.py
@@ -6,6 +6,13 @@ from pyscope import pyscope
 width = 640
 height = 480
 
+lifeP1 = 100
+lifeP2 = 100
+lifeP3 = 100
+
+time = 99
+pygame.time.set_timer(USEREVENT+1, 1000)
+
 if (sys.platform == "darwin"):
     screen = pygame.display.set_mode((width, height))
 else:
@@ -13,9 +20,9 @@ else:
     screen = scope.screen
     width = screen.get_size()[0]
     height = screen.get_size()[1]
+
 clock = pygame.time.Clock()
 background = pygame.Surface(screen.get_size())
-print screen.get_size()[0]
 background = background.convert()
 background.fill((250, 250, 250))
 background_img = pygame.image.load("grass.png").convert()
@@ -97,12 +104,15 @@ all_sprites_list.add(*pads)
 
 pygame.font.init()
 basicfont = pygame.font.Font('data/coders_crux/coders_crux.ttf', 20)
+bigfont = pygame.font.Font('data/coders_crux/coders_crux.ttf', 70)
 
 
 while 1:
     # USER INPUT
     deltat = clock.tick(30)
     for event in pygame.event.get():
+        if event.type == USEREVENT+1:
+            time -= 1
         if not hasattr(event, 'key'): continue
         down = event.type == KEYDOWN
         if crash == False:
@@ -112,25 +122,53 @@ while 1:
             elif event.key == K_DOWN: car.k_down = down * -2
             elif event.key == K_SPACE: car.k_space = down * 0
         if event.key == K_ESCAPE: pygame.quit()
+
+    if time == 0:
+        print "TIME OVER"
+
     # RENDERING
     screen.blit(background_img,(0,0))
     pad_group.clear(screen, background)
     car_group.clear(screen, background)
-    
+
+    # Display OSD
+    pygame.draw.rect(screen,(0,0,0), (0,0,60,height), 0)
+
+    # life P1
+    if lifeP1 >0:
+        pygame.draw.rect(screen,(255,0,0), (10, height-10, 10, -int(lifeP1/100.0*height/2)), 0)
+
+    # life P2
+    if lifeP2 >0:
+        pygame.draw.rect(screen,(0,255,0), (25, height-10, 10, -int(lifeP2/100.0*height/2)), 0)
+
+    # life P3
+    if lifeP3 >0:
+        pygame.draw.rect(screen,(0,0,255), (40, height-10, 10, -int(lifeP3/100.0*height/2)), 0)    
+
     # display fps
-    text = basicfont.render("%d fps" % int(clock.get_fps()), True, (255, 0, 0), (255, 255, 255))
+    text = basicfont.render("%d fps" % int(clock.get_fps()), True, (255, 0, 0), (0, 0, 0))
     textrect = text.get_rect()
-    textrect.centerx = text.get_rect().width /2
+    textrect.centerx = text.get_rect().width /2 + 5
     textrect.centery = 10
-    screen.fill((255, 255, 255))
     screen.blit(text, textrect)
+
+    # display time
+    if (time >= 0):
+        time_text = bigfont.render("%d" % time, True, (255, 255, 255), (0, 0, 0))
+        textrect = text.get_rect()
+        textrect.centerx = text.get_rect().width /2 + 5
+        textrect.centery = 50
+        screen.blit(time_text, textrect)
+
+
 
     car_group.update(deltat)
     # CHECK IF CAR IS LEAVING SCREEN
-    if car.position[0] <0:
+    if car.position[0] <90:
         car.position = (width, car.position[1])
     elif car.position[0] > width:
-        car.position = (0, car.position[1])
+        car.position = (90, car.position[1])
     if car.position[1] <0:
         car.position = (car.position[0], height)
     elif car.position[1] > height:
@@ -141,8 +179,12 @@ while 1:
     all_sprites_list.draw(screen)
     pads = pygame.sprite.spritecollide(car, pad_group, False)
     if pads:
-        car.src_image = car.car_hit
-        car.speed = 0
-        car.k_left = car.k_right = car.k_down = car.k_up = 0
-        crash = True
+        lifeP1=lifeP1-10
+        if lifeP1 != 0:
+            car.speed = -car.speed
+        else:
+            car.src_image = car.car_hit
+            car.speed = 0
+            car.k_left = car.k_right = car.k_down = car.k_up = 0
+            crash = True
     pygame.display.flip()
